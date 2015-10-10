0744d160d0068aaba480a9473e1ae9e170210061
Konrad Janica <konradjanica@gmail.com>
2015-10-10 17:04:03 +1030

    ready for api

--- Visualization/Site/js/infinity.js
+++ Visualization/Site/js/infinity.js
@@ -1,15 +1,19 @@
-var images = [];
+var images = ["6.jpg", "7.jpg"];
+var offset = 0;
 
-function addImage() {
-
+function addImage(img) {
+    images.push(img);
+    document.getElementById('imgDiv').appendChild(img);
 }
 
 function loadImages(){
-    for(var i=0; i< images.length; i++) {
+    for(var i = 0; i < images.length; i++) {
         var img = new Image();
             img.src = images[i];
         document.getElementById('imgDiv').appendChild(img);
+        linebreak = document.createElement("br");
+        document.getElementById('imgDiv').appendChild(linebreak);
     }
 }
 
-loadImages(images);
+loadImages();
